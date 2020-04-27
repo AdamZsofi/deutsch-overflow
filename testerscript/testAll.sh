55dbf792f92eb3f090d383a51f1bcf1942434fc0
AdamZsofi <adamzsofi.mail@gmail.com>
2020-04-27 22:56:17 +0200

    Moved tester script

--- testerscript/testAll.sh
+++ testerscript/testAll.sh
@@ -5,7 +5,7 @@
 # es a diff kimenetet a megfelelo fajlokba iranyitja, ahol ez visszaolvashato
 
 # Ezt ird at oda, ahol a repo van
-cd /home/solarowl/Egyetem_targyak_4/projlab/deutsch-overflow
+cd /home/solarowl/Egyetem_targyak_4/projlab/deutsch-overflow || exit
 
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13
 do
